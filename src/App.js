6c4067603447faf4b9e7e746f05e93019b945ec6
Pankaj8839 <pankajrajput8839@gmail.com.>
2024-12-03 17:36:58 +0530

    add file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+ const [formData, setFormData] = useState({
+   username: '',
+   password: '',
+ });
+ const [susess, setSusess] = useState(false);
+ const [error, setError] = useState("");
+  const hasFormSubmit = (e) => {
+    e.preventDefault();
+    if(formData.username === 'user' && formData.password === 'password') {
+      setSusess(true);
+    }else{
+      setError("Invalid Username or Password");
+    }
+  }
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    setFormData((prevFormData) => {
+      return {
+        ...prevFormData,
+        [name]: value,
+      };
+    });
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+    
+      <h1>Login Page</h1>
+      {error!=="" && !susess  && <p>{error}</p>}
+      {susess ? (<p>Welcome User!</p>):( <form onSubmit={(e)=>hasFormSubmit(e)} className="form"> 
+        <label>Username:<input type="text" placeholder="Username" name='username' value={formData.username} onChange={handleChange} required/></label>
+        <label>Password:<input type="password" placeholder="Password" name='password' value={formData.password} onChange={handleChange} required/></label>
+        <button type="submit">Login</button>
+      </form>)}
+     
     </div>
   );
 }
