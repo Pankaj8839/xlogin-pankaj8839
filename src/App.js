c8564789bd9dae5511c9deba5d524b4f1036e35b
Pankaj8839 <pankajrajput8839@gmail.com.>
2024-12-03 18:15:33 +0530

    see

--- src/App.js
+++ src/App.js
@@ -32,8 +32,8 @@ function App() {
       <h1>Login Page</h1>
       {error!=="" && !susess  && <p>{error}</p>}
       {susess ? (<p>Welcome User!</p>):( <form onSubmit={(e)=>hasFormSubmit(e)} className="form"> 
-        <label>Username:<input type="text" placeholder="username" name='username' value={formData.username} onChange={handleChange} required/></label>
-        <label>Password:<input type="password" placeholder="password" name='password' value={formData.password} onChange={handleChange} required/></label>
+        <label>Username:</label><input type="text" placeholder="username" name='username' value={formData.username} onChange={handleChange} required/><br/>
+        <label>Password:</label><input type="password" placeholder="password" name='password' value={formData.password} onChange={handleChange} required/><br/>
         <button type="submit">Submit</button>
       </form>)}
      
